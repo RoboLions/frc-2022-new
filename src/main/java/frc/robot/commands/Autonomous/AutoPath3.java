0ce42633f74ad6b5f94984a96bba48615e8a4086
Maika <maika.r@prhsrobotics.com>
2022-02-07 18:04:01 -0500

    AutoPath3 Updated

--- src/main/java/frc/robot/commands/Autonomous/AutoPath3.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoPath3.java
@@ -6,25 +6,27 @@ import frc.robot.commands.AutoMove;
 import frc.robot.commands.AutoShoot;
 import frc.robot.commands.StopNWait;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoPath3 {
     
-    public AutoPath3(final DriveSubsystem subsystem){
+    public AutoPath3(final DriveSubsystem subsystem, IntakeSubsystem intakesubsystem, ShooterSubsystem shootsubsystem){
 
     //consider lidar aiming
       new AutoMove(subsystem, null, 2.577, 5);
       new StopNWait(subsystem, 0.2);
-      new AutoIntake(true);
+      new AutoIntake(intakesubsystem);
       new StopNWait(subsystem, 0.2);
       //consider aiming
-      new AutoShoot(true);
+      new AutoShoot(shootsubsystem);
       new StopNWait(subsystem, 0.2);
       new AutoMove(subsystem, null,6.711,5);
       new StopNWait(subsystem, 0.2);
-      new AutoIntake(true);
+      new AutoIntake(intakesubsystem);
       new StopNWait(subsystem, 0.2);
       //consider aiming
-      new AutoShoot(true);
+      new AutoShoot(shootsubsystem);
       new StopNWait(subsystem, 0.2);
 
 
