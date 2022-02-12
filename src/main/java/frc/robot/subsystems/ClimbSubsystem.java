07567f40354c619bf403b0f4b2574d9a86ef931e
Maika <maika.r@prhsrobotics.com>
2022-02-12 12:38:25 -0500

    climb subsystem pid error fixed

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -10,12 +10,15 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.Constants.ClimbConstants;
+import frc.robot.lib.RoboLionsPID;
 
 public class ClimbSubsystem extends SubsystemBase {
   /** Creates a new ClimbSubsystem. */
  
   private static WPI_TalonSRX climbMotor = RobotMap.climbMotor;
   public double climb_enc_readout = 0;
+  
+  public RoboLionsPID climbPID = new RoboLionsPID();
 
 
   public ClimbSubsystem() {
@@ -24,6 +27,17 @@ public class ClimbSubsystem extends SubsystemBase {
 
   climbMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
   resetEncoder();
+
+  climbPID.initialize2(
+        0.0, // Proportional Gain
+        0.0, // Integral Gain 
+        0.0, // Derivative Gain
+        0.0, // Cage Limit 
+        0.0, // Deadband 
+        12,// MaxOutput Volts TODO: max output voltage value
+        false, //enableCage
+        false //enableDeadband
+    );
   }
 
   public void moveClimbToPosition(double target_position) {
